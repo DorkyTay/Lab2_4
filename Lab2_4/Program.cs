09bfe9ac94ebeb1253961be1573241734f37afe0
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-30 09:12:47 -0400

    completede progame

--- Lab2_4/Program.cs
+++ Lab2_4/Program.cs
@@ -7,6 +7,63 @@ namespace Lab2_4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int myFirstNumber = 19;
+
+            int mySecondNumber = 224;
+
+            string myText = "Some Text";
+
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+
+            string myText = " Some Text";
+
+            Console.WriteLine(myText + mySecondNumber);
+            Console.WriteLine(myText + " " + mySecondNumber);
+
+            string moreText = "Goes Here";
+
+            Console.WriteLine(myText + " " + moreText);
+            Console.WriteLine($"{myText} {moreText}");
+
+            string color1 = "Yellow";
+            string color2 = "Red";
+
+            bool isYellow = true;
+            bool isGreen = false;
+
+            Console.WriteLine(color1 + " " + isYellow + " " + color2 + " " + isGreen);
+            Console.WriteLine($"{color1} {isYellow} {color2} {isGreen}");
+
+            double numbers;
+
+            numbers = mySecondNumber / myFirstNumber;
+
+            Console.WriteLine(numbers);
+
+            numbers = mySecondNumber / (double)myFirstNumber;
+
+            Console.WriteLine(numbers);
+
+            Console.WriteLine("Breakpoint Here");
+
+            string textToNumber = "80473";
+
+            int results = 99 * int.Parse(textToNumber);
+
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+
+
+
+
+
+
+
+ 
+
+
+
         }
     }
 }
